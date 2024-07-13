0822a2f442cebabf6caa7160129636840ac50bd4
anasbm1000 <pkd19cs009@gecskp.ac.in>
2024-07-13 22:24:26 +0530

    Passed states as values to appropriate components

---
+++ cvusingcomponentsandprops/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import React, { Component } from 'react';
+import Personaldetails from './component/personaldetails';
+import Summary from './component/summary';
+import Educationdetails from './component/educationdetails';
+import Projects from './component/projects';
+import Internships from './component/internships';
+import Skillset from './component/skillset';
+
+class App extends Component {
+  state = {
+    name: "Anas bin Malik",
+    jobRole: "Front end Developer",
+    place: "Mannarkkad",
+    city: "Palakkad",
+    phone: "7382917301",
+    emailID: "anasbinmalik1000@gmail.com",
+    github: "https://github.com/anasbm1000",
+    linkedin: "https://www.linkedin.com/in/anasbm1000/",
+    college1: "GEC Thrissur(APJAKTU), Thrissur, India",
+    college1CGPA: "7.94",
+    college2: "GEC Palakkad(APJAKTU), Palakkad, India",
+    college2CGPA: "8.34",
+    college3: "DHSS Nellipuzha(State Board), Palakkad, India",
+    college3CGPA: "97.75%",
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Personaldetails
+          name={this.state.name}
+          place={this.state.place}
+          city={this.state.city}
+          jobRole={this.state.jobRole}
+          phone={this.state.phone}
+          emailID={this.state.emailID}
+          github={this.state.github}
+          linkedin={this.state.linkedin}
+        />
+        <Summary />
+        <Educationdetails
+          college1={this.state.college1}
+          college1CGPA={this.state.college1CGPA}
+          college2={this.state.college2}
+          college2CGPA={this.state.college2CGPA}
+          college3={this.state.college3}
+          college3CGPA={this.state.college3CGPA}
+        />
+        <Projects />
+        <Internships />
+        <Skillset />
+      </div>
+    );
+  }
+}
+
+export default App;
