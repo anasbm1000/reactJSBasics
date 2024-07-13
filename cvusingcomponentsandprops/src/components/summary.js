5694f0332e8a131631dcb1e04496c47edf1e162e
anasbm1000 <pkd19cs009@gecskp.ac.in>
2024-07-13 22:34:07 +0530

    Added summary in CV by passing props

---
+++ cvusingcomponentsandprops/src/components/summary.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Summary extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          I'm a Postgraduate student in Computer Science Engineering with an immense enthusiasm towards technology, looking for an entry-level job position where the ability to use education and hard work can forge opportunities to revitalise.
+        </p>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default Summary;
