7f49d2e437e217e0bf0ecf957819001e7e67f131
anasbm1000 <pkd19cs009@gecskp.ac.in>
2024-07-22 06:14:24 +0530

    Created 2 banners to project company acheivements

---
+++ dreamhome/src/Components/aboutus.js
@@ -0,0 +1,17 @@
+import React from 'react';
+class Aboutus extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="banner">
+                    <img src="./aboutus.png" width={1500} className='banner' alt='college' />
+                </div>
+                <div className='banner'>
+                    <img src="./banner.png" width={1500} className='banner' alt='college' />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Aboutus;
