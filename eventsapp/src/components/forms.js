a025634c9768bfefec8e3d506aef45db859aef00
anasbm1000 <pkd19cs009@gecskp.ac.in>
2024-07-22 06:13:13 +0530

    Provided a card view to fetch the values

--- eventsapp/src/components/forms.js
+++ eventsapp/src/components/forms.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import '../App.css';
 
@@ -19,8 +20,8 @@ class Forms extends React.Component {
     }
 
     content=event=>{
-        console.log("typing...")
-        console.log(this.state.firstname)
+        console.log("User is typing - " + event.target.name)
+        //console.log(this.state.firstname)
         console.log(event.target.value)
         // this.setState({firstname:event.target.value})
         this.setState({
@@ -28,68 +29,82 @@ class Forms extends React.Component {
         })
     }
 
-  
+    
 
     submission=()=>{
         console.log("registered")
         console.log(this.state)
     }
+
+    
     render() {
         return (
-            
-            <form action="" method="post" className='App-header'>
-                
-                <table>
-                    {/* <tr>
-                        <th></th>
-                        <th><h1>Registration</h1></th>
-                    </tr> */}
-                    <tr>
-                        <th>First Name</th>
-                        <td><input type="text" name='firstname'  value={this.state.firstname} onChange={this.content} placeholder='First Name' /></td>
-                        <th>Last Name</th>
-                        <td><input type="text" name='lastname' value={this.state.lastname} onChange={this.content} placeholder='Last Name' /></td>
-                    </tr>
-                    <tr>
-                        <th>Email</th>
-                        <td><input type="email" name='email' value={this.state.email} onChange={this.content} placeholder='Email' /></td>
-                        <th>Phone</th>
-                        <td><input type="number" name='phone' value={this.state.phone} onChange={this.content} placeholder='Phone' /></td>
-                    </tr>
-                    <tr>
-                        <th>Gender</th>
-                        <td><input type="radio" name='gender' value={this.state.gender} onChange={this.content} />Male
-                        <input type="radio" name='gender' value={this.state.gender} onChange={this.content} />Female</td>
-                        <th>Date of Birth</th>
-                        <td><input type="date" name='dob' value={this.state.dob} onChange={this.content} /></td>
-                    </tr>
+            <div>
+                <form action="" method="post" className='App-header'>
                     
-                    <tr>
-                        <th>City</th>
-                        <td><input type="text" name='city' value={this.state.city} onChange={this.content} /></td>
-                        <th>State</th>
-                        <td><input type="text" name='state' value={this.state.state} onChange={this.content} /></td>
-                    </tr>
-                    <tr>
-                        <th>Country</th>
-                        <td><input type="text" name='country' value={this.state.country} onChange={this.content} /></td>
-                        <th>Pincode</th>
-                        <td><input type="number" name='pincode' value={this.state.pincode} onChange={this.content} /></td>
-                    </tr>
-                    <tr>
-                        <th>Username</th>
-                        <td><input type="text" name='username' value={this.state.username} onChange={this.content} /></td>
-                        <th>Password</th>
-                        <td><input type="password" name='password' value={this.state.password} onChange={this.content} /></td>
-                    </tr>
-                    <tr>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th><button type='button' onClick={this.submission}>Register</button></th>
-                    </tr>
-                </table>
-            </form>
+                    <table>
+                        {/* <tr>
+                            <th></th>
+                            <th><h1>Registration</h1></th>
+                        </tr> */}
+                        <tr>
+                            <th>First Name</th>
+                            <td><input type="text" name='firstname'  value={this.state.firstname} onChange={this.content} placeholder='First Name' /></td>
+                            <th>Last Name</th>
+                            <td><input type="text" name='lastname' value={this.state.lastname} onChange={this.content} placeholder='Last Name' /></td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td><input type="email" name='email' value={this.state.email} onChange={this.content} placeholder='Email' /></td>
+                            <th>Phone</th>
+                            <td><input type="number" name='phone' value={this.state.phone} onChange={this.content} placeholder='Phone' /></td>
+                        </tr>
+                        <tr>
+                            <th>Gender</th>
+                            <td><input type="radio" name='gender' value={this.state.gender} onChange={this.content} />Male
+                            <input type="radio" name='gender' value={this.state.gender} onChange={this.content} />Female</td>
+                            <th>Date of Birth</th>
+                            <td><input type="date" name='dob' value={this.state.dob} onChange={this.content} /></td>
+                        </tr>
+                        
+                        <tr>
+                            <th>City</th>
+                            <td><input type="text" name='city' value={this.state.city} onChange={this.content} /></td>
+                            <th>State</th>
+                            <td><input type="text" name='state' value={this.state.state} onChange={this.content} /></td>
+                        </tr>
+                        <tr>
+                            <th>Country</th>
+                            <td><input type="text" name='country' value={this.state.country} onChange={this.content} /></td>
+                            <th>Pincode</th>
+                            <td><input type="number" name='pincode' value={this.state.pincode} onChange={this.content} /></td>
+                        </tr>
+                        <tr>
+                            <th>Username</th>
+                            <td><input type="text" name='username' value={this.state.username} onChange={this.content} /></td>
+                            <th>Password</th>
+                            <td><input type="password" name='password' value={this.state.password} onChange={this.content} /></td>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th><button type='button' onClick={this.submission}>Register</button></th>
+                        </tr>
+                    </table>
+                </form>
+                <div className="card-container">
+                    
+                        <div className="card">
+                            <h3>{this.state.firstname}</h3>
+                            <p>State: {this.state.state}</p>
+                            <p>City: {this.state.city}</p>
+                            <p>Email: {this.state.email}</p>
+                            <p>Phone: {this.state.phone}</p>
+                        </div>
+                    
+                </div>
+            </div>    
         );
     }
 }
