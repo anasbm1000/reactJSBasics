da35de83fe0264d53d6c9cc1ae3c70ac8027cfd2
anasbm1000 <pkd19cs009@gecskp.ac.in>
2024-08-03 10:59:08 +0530

    Implemented the topmost component of a chat interface

---
+++ chatinterface/src/Component/Header/header.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import {Link} from 'react-router-dom';
+
+// import '../../App.css';
+
+
+// const Header = () => {
+//     return (
+//         <div>
+//             <div className='nav'>
+//                 <div className='fHeading'>
+//                     <Link to = "/"><img src="./left.png" height={20} alt="" /></Link>
+//                     <img src="./dp.png" height={25} alt="" />
+//                     <h4 id = "welcometag"> Riss Technologies </h4>
+//                 </div>
+//                 <div className='links'>
+//                     <Link to = "/"> <img src="./videocall.png" height={20} alt="" /> </Link>
+//                     <Link to = "/ad"> <img src="./voicecall.png" height={20} alt="" /> </Link>
+//                     <Link to = "/service"> <img src="./search.png" height={20} alt="" /> </Link>                 
+//                 </div>
+//             </div>
+//         </div>
+//     )
+               
+// }
+
+// export default Header;
