07c45261a078219900794f5a7f0fc54088f1f295
anasbm1000 <pkd19cs009@gecskp.ac.in>
2024-08-07 12:34:51 +0530

    Left a space for upgrading navbar in near-future

--- expensetracker/src/components/Home.js
+++ expensetracker/src/components/Home.js
@@ -3,17 +3,22 @@ import { Link } from 'react-router-dom';
 import '../App.css';
 
 const Home = () => (
-  <div className="home">
-    <h2>Welcome to Expense Tracker</h2>
-    <div className="card-container">
-      <div className="card">
-        <Link to="/profile">Profile</Link>
-      </div>
-      <div className="card">
-        <Link to="/income">Add Income</Link>
-      </div>
-      <div className="card">
-        <Link to="/expenses">Calculate Expenses</Link>
+  <div>
+    <div>
+
+    </div>
+    <div className="home">
+      <h2>Welcome to <span id="expense"> Expense </span> Tracker</h2>
+      <div className="card-container">
+        <div className="card">
+          <Link to="/profile">Profile</Link>
+        </div>
+        <div className="card">
+          <Link to="/income">Add Income</Link>
+        </div>
+        <div className="card">
+          <Link to="/expenses">Calculate Expenses</Link>
+        </div>
       </div>
     </div>
   </div>
