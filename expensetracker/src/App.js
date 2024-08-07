261e8c61cdd28fc98318c2fd42c2c58305f2f88e
anasbm1000 <pkd19cs009@gecskp.ac.in>
2024-08-07 12:33:29 +0530

    Made income status responsive by fetching value from Income.js

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Footer from './components/Footer';
 import Income from './components/Income';
+import Expenses from './components/Expenses';
 
 import './App.css';
-const App = () => (
-  <Router>
-    <main>
-      <h1>Your available balance is: <span id="expense">Rs 15000</span></h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/income" element={<Income />} />
-      </Routes>
-    </main>
-    <Footer />
-  </Router>
-);
+
+const App = () => {
+  const [income, setIncome] = useState();
+
+  useEffect(() => {
+    const storedIncome = localStorage.getItem('income');
+    if (storedIncome) {
+      setIncome(Number(storedIncome));
+    }
+  }, []);
+
+  const handleSetIncome = (newIncome) => {
+    setIncome(newIncome);
+    localStorage.setItem('income', newIncome);
+  };
+
+  return (
+    <Router>
+      <h1>Your available balance is: <span id="expense">Rs {income}</span></h1>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/income" element={<Income income={income} setIncome={handleSetIncome} />} />
+          <Route path="/expenses" element={<Expenses />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
-
-
-// 
-// import Expenses from './components/Expenses';
-
-// 
-// <Route path="/expenses" element={<Expenses />} />
